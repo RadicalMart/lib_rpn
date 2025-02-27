f7cb23dc29c9725e518c146bd37c156c9956b452
dmitriitsymbal <tsymbal.mitia@gmail.com>
2025-02-27 17:09:46 +0300

    удалил мусор и добавил gitignore

--- tests/solve.php
+++ tests/solve.php
@@ -12,11 +12,16 @@ $formula = new \RPN\Formula();
 
 $formula->getFunctions()
     ->register(\RPN\StandardMathFunctions::class)
-    ->register(\RPN\StringFunctions::class);
+    ->register(\RPN\StringFunctions::class)
+    ->register('myfunc', 1, static function ($a) {
+        return (float)$a * 3;
+    });
 
 $rows = [
     '6*2+sqrt(4^sqrt(4^2))',
     'concat("1", "2")+5',
+    'sqrt(sqrt(2^2) + 5/2*4)',
+    'myfunc(5) + 3',
 ];
 
 foreach ($rows as $row) {
@@ -24,4 +29,3 @@ foreach ($rows as $row) {
     echo "Формула: " . $row . "\n";
     echo "Результат: " . $result . "\n\n";
 }
-
